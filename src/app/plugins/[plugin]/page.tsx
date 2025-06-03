c7da931a3699b9aadef200662168648e902a0be3
Zeke142 <dirthaul142@gmail.com>
2025-06-03 16:27:41 -0700

    Update page.tsx

--- src/app/plugins/[plugin]/page.tsx
+++ src/app/plugins/[plugin]/page.tsx
@@ -3,15 +3,15 @@
 import dynamic from 'next/dynamic'
 import { useParams } from 'next/navigation'
 
-const pluginMap: Record<string, any> = {
-  example: dynamic(() => import('../example/index'), { ssr: false }),
-  map: dynamic(() => import('../map/index'), { ssr: false }),
-  profile: dynamic(() => import('../profile/index'), { ssr: false }),
+const pluginMap: Record<string, unknown> = {
+  example: dynamic(() => import('@/app/plugins/example/index'), { ssr: false }),
+  map: dynamic(() => import('@/app/plugins/map/index'), { ssr: false }),
+  profile: dynamic(() => import('@/app/plugins/profile/index'), { ssr: false }),
 }
 
 export default function PluginPage() {
   const { plugin } = useParams()
-  const PluginComponent = pluginMap[plugin as string]
+  const PluginComponent = pluginMap[plugin as string] as React.ElementType
 
   if (!PluginComponent) {
     return <div className="p-4 text-red-600">Plugin not found: {plugin}</div>
