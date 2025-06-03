c7db971bb169ae408ae5c715b36739dca97a9051
Zeke 142 <diryhaul142@gmail.com>
2025-06-03 15:17:29 -0700

    Deploy: working plugin system

--- src/app/plugins/[plugin]/page.tsx
+++ src/app/plugins/[plugin]/page.tsx
@@ -3,15 +3,17 @@
 import dynamic from 'next/dynamic'
 import { useParams } from 'next/navigation'
 
+const pluginMap = {
+  example: dynamic(() => import('../../../../plugins/example/index'), { ssr: false }),
+}
+
 export default function PluginPage() {
   const { plugin } = useParams()
-  const PluginComponent = dynamic(
-    () => import(`../../../plugins/${plugin}/index`),
-    {
-      loading: () => <p>Loading plugin...</p>,
-      ssr: false,
-    }
-  )
+  const PluginComponent = pluginMap[plugin]
+
+  if (!PluginComponent) {
+    return <div className="p-4 text-red-600">Plugin not found: {plugin}</div>
+  }
 
   return (
     <div className="p-4">
