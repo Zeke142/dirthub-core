6f3026d3179ecafa207feef83167f59c1df96862
Zeke142 <dirthaul142@gmail.com>
2025-06-05 18:35:33 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
-module.exports = {
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
   darkMode: 'class',
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
-    extend: {
-      colors: {
-        dirtbrown: '#7a5c39'
-      }
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
+export default config
