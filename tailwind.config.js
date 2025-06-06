26d1795942d05c7d7015ef8527374b6dd6dcd7b4
Zeke142 <dirthaul142@gmail.com>
2025-06-06 16:46:05 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
-    './src/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}'
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-  darkMode: 'media',
-};
+}
