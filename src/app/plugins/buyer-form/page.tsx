0174e58ed7ee517d98a4dd55eb2412227274acac
Zeke142 <dirthaul142@gmail.com>
2025-06-07 14:37:25 -0700

    Create page.tsx

---
+++ src/app/plugins/buyer-form/page.tsx
@@ -0,0 +1,8 @@
+export default function BuyerFormPage() {
+  return (
+    <div className="p-4 bg-white shadow rounded plugin-buyer-form">
+      <h2 className="text-lg font-semibold">Buyer Dirt Request</h2>
+      <p>This plugin collects fill dirt needs by location and volume.</p>
+    </div>
+  );
+}
